0bbde4496eca52ac50c0028ad5685514f37977c7
kmc-jhonjoel <jhon.cabagua@kmc.solutions>
2022-10-17 16:54:49 +0800

    chore(setup): project setup with eslint, prettier, husky, commitlint, commitizen, semantic release

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
