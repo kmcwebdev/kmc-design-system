24a4e4f79d20e7fce841945ecb8675031d5689ce
kmc-jhonjoel <jhon.cabagua@kmc.solutions>
2022-10-17 18:55:24 +0800

    feat(tailwindcss): initial setup for tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <div className="text-6xl text-red-600">{count}</div>
+        <button
+          className="px-6 py-2 rounded bg-green-800 hover:ring-2 hover:bg-green-600 text-white"
+          type="button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count+
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
