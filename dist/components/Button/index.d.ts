e19673b895a2a53f14cdbe9305afc0c895f950f1
kmc-jhonjoel <jhon.cabagua@kmc.solutions>
2022-10-18 11:25:52 +0800

    fix(package.json): fix npm build output

--- dist/components/Button/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export declare type ButtonVariant = 'filled-primary' | 'filled-secondary' | 'filled-success' | 'filled-warning' | 'filled-danger' | 'filled-info' | 'outlined-primary' | 'outlined-secondary' | 'outlined-success' | 'outlined-warning' | 'outlined-danger' | 'outlined-info';
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    variant?: ButtonVariant;
-    size?: 'sm' | 'base';
-}
-export declare const Button: React.FC<ButtonProps>;
-//# sourceMappingURL=index.d.ts.map
