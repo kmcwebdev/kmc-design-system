a15ddf7b6c87dd9f81d070b2c2dcf3fa4cf83990
jyzrgandia <73985156+jyzrgandia@users.noreply.github.com>
2022-10-25 10:26:00 +0800

    docs(PR template): added template on every pr (#26)
    * docs(PR template): added template on every pr
    
    * docs(changed vite config): updated cite configuration file
    
    Signed-off-by: Jhon Joel Cabagua Jr. <57856474+joelcabagua@users.noreply.github.com>
    Co-authored-by: Jhon Joel Cabagua Jr <57856474+joelcabagua@users.noreply.github.com>

---
+++ .github/pull_request_template.md
@@ -0,0 +1,14 @@
+## Description
+
+Please include a summary of the change and which issue is fixed. Please also include relevant motivation and context. List any dependencies that are required for this change.
+
+## Type of change
+
+This pull request contains:
+
+- [ ] New Feature
+- [ ] Component Updates
+- [ ] Bug Fix
+- [ ] Code Refactor
+- [ ] Storybook Updates
+- [ ] Other (about what?)
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import EsLint from 'vite-plugin-linter';
-// import tsConfigPaths from 'vite-tsconfig-paths';
-const { EsLinter, linterPlugin } = EsLint;
 import * as packageJson from './package.json';
+const { EsLinter, linterPlugin } = EsLint;
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [
     react(),
-    // tsConfigPaths(),
     linterPlugin({
       include: ['./src}/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
