bcd9bb016394c43d6c99f08497ee9a1cf3d3c0ea
jyzrgandia <jayzur.gandia@kmc.solutions>
2022-10-25 09:48:12 +0800

    docs(PR template): added template on every pr

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import EsLint from 'vite-plugin-linter';
-import tsConfigPaths from 'vite-tsconfig-paths';
-const { EsLinter, linterPlugin } = EsLint;
+// import tsConfigPaths from 'vite-tsconfig-paths';
 import * as packageJson from './package.json';
-import * as path from 'path';
+const { EsLinter, linterPlugin } = EsLint;
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [
     react(),
-    tsConfigPaths(),
+    // tsConfigPaths(),
     linterPlugin({
       include: ['./src}/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
