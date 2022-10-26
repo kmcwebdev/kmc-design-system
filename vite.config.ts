8d154fabd14c49b5374e9103277d1abacc59e84a
joelcabagua <cabagua.joel@gmail.com>
2022-10-26 10:44:37 +0800

    fix(bundle size): reduce bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import react from '@vitejs/plugin-react';
-import { resolve } from 'node:path';
+import * as packageJson from './package.json';
+
+import EsLint from 'vite-plugin-linter';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import EsLint from 'vite-plugin-linter';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 // import tsConfigPaths from 'vite-tsconfig-paths';
 const { EsLinter, linterPlugin } = EsLint;
-import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
