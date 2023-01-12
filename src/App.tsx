bee28b5bd58b4b0d002738a20fe0e770389c3d0f
joelcabagua <cabagua.joel@gmail.com>
2023-01-12 10:28:45 +0800

    refactor: update dependencies and starter files for sidenav

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div className="h-screen w-screen text-center">
-      <h1>KMC Design System</h1>
+      <h1>KMC Design System</h1>a sdasdas
+      <div className="h-screen">asdasds</div>
     </div>
   );
 }
