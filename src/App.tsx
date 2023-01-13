fe3fa472216110a33c87c1dce7a9cdd6ea09b743
joelcabagua <cabagua.joel@gmail.com>
2023-01-13 10:26:28 +0800

    fix: removed unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import React, { useState } from 'react';
-import { Sidebar } from './component';
-import RectangleGroupIcon from '@heroicons/react/24/solid/RectangleGroupIcon';
-import ArrowLongUpIcon from '@heroicons/react/24/solid/ArrowLongUpIcon';
+import React from 'react';
 
 function App() {
-  const [activeNav, setActiveNav] = useState('');
-
   return (
     <div className="h-screen w-screen text-center bg-gray-50">
       <>KMC Design System</>
-      <Sidebar className="w-[88px]" theme="dark" onChange={(e) => setActiveNav(e)} activeNav={activeNav}>
-        <Sidebar.Item icon={<RectangleGroupIcon className="h-6 w-6" />} id="/dashboard">
-          Dashboard
-        </Sidebar.Item>
-        <Sidebar.Item icon={<ArrowLongUpIcon className="h-6 w-6" />} id="/profile">
-          Profile
-        </Sidebar.Item>
-      </Sidebar>
     </div>
   );
 }
